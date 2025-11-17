6d10c7af20c762ea405bb01d987e3c33caefb27c
Sam-0505 <samirpatro2001@gmail.com>
2025-11-17 00:43:02 -0600

    model saving

--- train.py
+++ train.py
@@ -198,7 +198,7 @@ def main():
             logging.info('train: epoch %3d: batch %3d: loss %f', epoch, batch_idx, loss)
 
         logging.info('train: epoch %3d: average_loss %f', epoch, np.average(losses))
-        if (epoch + 1) % 5 == 0 or epoch == args.epochs - 1:
+        if (epoch + 1) % 10 == 0 or epoch == args.epochs - 1:
             logging.info('----------validation')
             utils.save(model, os.path.join(model_path, f'weights_{epoch}.pt'))
 
